7afc33bafe46e17843fcd90e266b5349fcdc5c4e
dahoo <dahoo@DESKTOP-SE8G5DJ>
2025-08-26 12:21:00 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Assignment 1.2.2 
+//Find sum of first 10 natural #s then print. 
+
+int sum = 0;
+Console.WriteLine("The first 10 natural numbers are: ");
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i + " ");
+    sum += i;
+}
+
+Console.WriteLine("\nThe sum of first 10 natural #: " + sum);
+    
+    
+        
+
+
+ 
